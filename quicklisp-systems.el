6bfa0261377e482991c92680c685d6035e58819e
Mariano Montone <marianomontone@gmail.com>
2021-04-26 19:43:19 -0300

    Emacs mode preparations

--- quicklisp-systems.el
+++ quicklisp-systems.el
@@ -1,22 +1,38 @@
-(defun quicklisp-apropos-systems (string)
+;;; quicklisp-systems --- Utilities for querying Quicklisp systems.
+;;; Commentary:
+;;; Code:
+
+(require 'slime)
+
+(defun quicklisp-apropos-systems (pattern)
+  "Apropos Quicklisp systems."
   (interactive)
   )
 
-(defun quicklisp-apropos-systems-names (string)
+(defun quicklisp-apropos-systems-names (pattern)
+  "Apropos Quicklisp systems by name."
   (interactive)
   )
 
 (defun quicklisp-load (system-name)
+  "Load Quicklisp system."
   (interactive))
 
 (defalias 'quickload 'quicklisp-load)
 
 (defun quicklisp-systems-download ()
+  "Download quicklisp-systems list."
   (interactive))
 
 (defun quicklisp-systems ()
+  "Show a buffer with all quicklisp systems"
+  (interactive))
+
+(defun quicklisp-systems-update ()
+  "Update the list of Quicklisp systems."
   (interactive)
-  "Shows a buffer with all quicklisp systems"
-  )
+  (quicklisp-systems-download))
 
 (provide 'quicklisp-systems)
+
+;;; quicklisp-systems.el ends here
