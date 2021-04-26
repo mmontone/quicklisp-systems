ddc32d829c8054dd24b6f52fcc645caf6462150e
Mariano Montone <marianomontone@gmail.com>
2021-04-26 18:48:30 -0300

    Prepare for Emacs extension

---
+++ quicklisp-systems.el
@@ -0,0 +1,17 @@
+(defun quicklisp-apropos-systems (string)
+  (interactive)
+  )
+
+(defun quicklisp-apropos-systems-names (string)
+  (interactive)
+  )
+
+(defun quicklisp-load (system-name)
+  (interactive))
+
+(defalias 'quickload 'quicklisp-load)
+
+(defun quicklisp-systems-download ()
+  (interactive))
+
+(provide 'quicklisp-systems)
