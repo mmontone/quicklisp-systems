1023ba6b076ba2155e6f0b6560ecbe58564d04e1
Mariano Montone <marianomontone@gmail.com>
2021-04-26 20:20:13 -0300

    First working Emacs example

--- quicklisp-systems.el
+++ quicklisp-systems.el
@@ -2,21 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
+;; -*- lexical-binding: t -*-
+
 (require 'slime)
 
 (defun quicklisp-apropos-systems (pattern)
   "Apropos Quicklisp systems."
-  (interactive)
+  (interactive "sQuicklisp apropos:")
   )
 
 (defun quicklisp-apropos-systems-names (pattern)
   "Apropos Quicklisp systems by name."
-  (interactive)
+  (interactive "sQuicklisp apropos:")
   )
 
 (defun quicklisp-load (system-name)
   "Load Quicklisp system."
-  (interactive))
+  (interactive "sQuickload:")
+  (slime-eval `(ql:quickload ,system-name)))
 
 (defalias 'quickload 'quicklisp-load)
 
@@ -32,6 +35,37 @@
   "Update the list of Quicklisp systems."
   (interactive)
   (quicklisp-systems-download))
+
+(defun quicklisp-systems--open-buffer ()
+  (let ((buffer (current-buffer)))
+    (setq buffer-read-only t)
+    (local-set-key "q" 'quicklisp-systems--kill-current-buffer)
+    (buffer-disable-undo)
+    (set (make-local-variable 'kill-buffer-query-functions) nil)
+    (goto-char 0)
+    (pop-to-buffer buffer)))
+
+(defun quicklisp-systems-show-system (system-name)
+  "Show Quicklisp system SYSTEM-NAME."
+  (interactive "sShow Quicklisp system:")
+  (let ((system (slime-eval `(quicklisp-systems::find-system-info ,system-name))))
+    (if (null system)
+	(error "Quicklisp system not found: %s" system-name)
+      (let ((buffer (get-buffer-create (format "*quicklisp-systems: %s*" system-name))))
+	(with-current-buffer buffer
+	  (insert (getf system :name))
+	  (newline 2)
+	  (when (getf system :description)
+	    (insert (getf system :description))
+	    (newline 2))
+	  (when (getf system :long-description)
+	    (insert (getf system :long-description))
+	    (newline 2))
+	  
+	  (quicklisp-systems--open-buffer))))))
+
+;; (quicklisp-systems-show-system "hunchentoot")
+;; (quicklisp-systems-show-system "ten")
 
 (provide 'quicklisp-systems)
 
