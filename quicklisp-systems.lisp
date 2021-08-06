29777f88c1ea6875e28cf8c578a3cfb9a71ae9cd
Mariano Montone <marianomontone@gmail.com>
2021-08-06 13:47:39 -0300

    Usage instructions

--- quicklisp-systems.lisp
+++ quicklisp-systems.lisp
@@ -13,6 +13,14 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;; Use:
+
+;; Clone quicklisp-projects and quicklisp-controller repositories.
+;; Setup quicklisp-controller: (quicklisp-controller:setup-directories "~/src/lisp/quicklisp-projects/")
+;; Update the list of Quicklisp systems using QUICKLISP-CONTROLLER:UPDATE-WHAT-YOU-CAN.
+;; Load all ASDF systems available in *QUICKLISP-CONTROLLER-DIRECTORY* using REGISTER-ALL-ASDF-FILES
+;; Then use WRITE-SYSTEMS-FILE to serialize to a QUICKLISP-SYSTEM distribution file.
+
 (require :dexador)
 (require :asdf)
 (require :quicklisp)
@@ -48,6 +56,7 @@ If INCLUDE-SUBDIRECTORIES is T, then work recursively."
       (find-files-do subdir pattern function include-subdirectories))))
 
 (defun register-all-asdf-files (&optional (quicklisp-controller-directory *quicklisp-controller-directory*))
+  "Load all ASDF system definition files found in QUICKLISP-CONTROLLER-DIRECTORY."
   (setf *failed-asdf-files* nil)
   (format *standard-output* "Finding ASDF files...~%")
   (find-files-do
@@ -69,29 +78,22 @@ If INCLUDE-SUBDIRECTORIES is T, then work recursively."
 	 ))))))
 
 (defun serialize-asdf-systems (systems stream)
+  "Serialize all ASDF SYSTEMS to STREAM."
   (loop for system in systems
         do
-        #+nil(prin1 (list `(:name ,(asdf:component-name system)
-                            :description ,(asdf/component:component-description system)
-                            :author ,(asdf/system:system-author system)
-                            :maintainer ,(asdf/system:system-maintainer system)
-                            :homepage ,(asdf/system:system-homepage system)
-                            :bug-tracker ,(asdf/system:system-bug-tracker system)
-                            :license ,(asdf/system:system-license system)))
-                    stream)
-             (prin1 `(:name ,(slot-value system 'asdf/component::name)
-                      :description ,(asdf/component:component-description system)
-                      :long-description ,(asdf/component:component-long-description system)
-                      :author ,(slot-value system 'asdf/system::author)
-                      :mailto ,(slot-value system 'asdf/system::mailto)
-                      :maintainer ,(slot-value system 'asdf/system::maintainer)
-                      :homepage ,(slot-value system 'asdf/system::homepage)
-                      :bug-tracker ,(slot-value system 'asdf/system::bug-tracker)
-                      :version ,(slot-value system 'asdf/system::version)
-                      :license ,(slot-value system 'asdf/system::licence)
-		      :depends-on ,(remove-if-not 'stringp (slot-value system 'asdf/system::depends-on)))
-                    stream)
-             (terpri stream)))
+	   (prin1 `(:name ,(slot-value system 'asdf/component::name)
+                    :description ,(asdf/component:component-description system)
+                    :long-description ,(asdf/component:component-long-description system)
+                    :author ,(slot-value system 'asdf/system::author)
+                    :mailto ,(slot-value system 'asdf/system::mailto)
+                    :maintainer ,(slot-value system 'asdf/system::maintainer)
+                    :homepage ,(slot-value system 'asdf/system::homepage)
+                    :bug-tracker ,(slot-value system 'asdf/system::bug-tracker)
+                    :version ,(slot-value system 'asdf/system::version)
+                    :license ,(slot-value system 'asdf/system::licence)
+		    :depends-on ,(remove-if-not 'stringp (slot-value system 'asdf/system::depends-on)))
+                  stream)
+           (terpri stream)))
 
 (defun write-systems-file (&optional (path *systems-file*))
   (with-open-file (f path :direction :output :external-format :utf-8
