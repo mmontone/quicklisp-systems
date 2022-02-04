8fbc75db3eadf40f42f57986a1f0f1ef7b56f9dd
Mariano Montone <marianomontone@gmail.com>
2022-02-04 08:56:26 -0300

    Fix compilation warnings.

--- quicklisp-systems.el
+++ quicklisp-systems.el
@@ -19,7 +19,6 @@
 ;;; Code:
 
 (require 'slime)
-(require 'cl)
 
 (defface quicklisp-systems-button
   '((t (:box (:line-width 2 :color "dark grey")
@@ -42,7 +41,9 @@
 
 (defcustom quicklisp-systems-search-url
   "https://www.google.com/search?q=common lisp "
-  "URL used to search a Lisp library on the internet.")
+  "URL used to search a Lisp library on the internet."
+  :type 'string
+  :group 'quicklisp-systems)
 
 (defun quicklisp-systems-search-on-the-internet (library-name)
   (browse-url (concat quicklisp-systems-search-url library-name)))
@@ -100,12 +101,12 @@
   "Kill all quicklisp-systems buffers at once."
   (interactive)
   (mapcar 'kill-buffer
-          (remove-if-not
+          (cl-remove-if-not
            (lambda (buffer)
              (string-prefix-p "*quicklisp-systems" (buffer-name buffer)))
            (buffer-list))))
 
-(defun* quicklisp-systems-apropos (pattern)
+(cl-defun quicklisp-systems-apropos (pattern)
   "Apropos Quicklisp systems."
   (interactive "sQuicklisp apropos: ")
   (quicklisp-systems--check-systems-list)
@@ -113,13 +114,13 @@
         (buffer-name (format "*quicklisp-systems: apropos %s*" pattern)))
     (when (get-buffer buffer-name)
       (pop-to-buffer buffer-name)
-      (return-from quicklisp-systems-apropos))
+      (cl-return-from quicklisp-systems-apropos))
     (let ((buffer (get-buffer-create buffer-name)))
       (with-current-buffer buffer
         (quicklisp-systems--print-systems-list systems)
         (quicklisp-systems--open-buffer)))))
 
-(defun* quicklisp-systems-apropos-name (pattern)
+(cl-defun quicklisp-systems-apropos-name (pattern)
   "Apropos Quicklisp systems by name."
   (interactive "sQuicklisp apropos system name: ")
   (quicklisp-systems--check-systems-list)
@@ -127,13 +128,13 @@
         (buffer-name (format "*quicklisp-systems: apropos name %s*" pattern)))
     (when (get-buffer buffer-name)
       (pop-to-buffer buffer-name)
-      (return-from quicklisp-systems-apropos-name))
+      (cl-return-from quicklisp-systems-apropos-name))
     (let ((buffer (get-buffer-create buffer-name)))
       (with-current-buffer buffer
         (quicklisp-systems--print-systems-list systems)
         (quicklisp-systems--open-buffer)))))
 
-(defun* quicklisp-systems-apropos-author (pattern)
+(cl-defun quicklisp-systems-apropos-author (pattern)
   "Apropos Quicklisp systems by author."
   (interactive "sQuicklisp apropos author: ")
   (quicklisp-systems--check-systems-list)
@@ -141,33 +142,34 @@
         (buffer-name (format "*quicklisp-systems: apropos author %s*" pattern)))
     (when (get-buffer buffer-name)
       (pop-to-buffer buffer-name)
-      (return-from quicklisp-systems-apropos-author))
+      (cl-return-from quicklisp-systems-apropos-author))
     (let ((buffer (get-buffer-create buffer-name)))
       (with-current-buffer buffer
         (quicklisp-systems--print-systems-list systems)
         (quicklisp-systems--open-buffer)))))
 
-(defun quicklisp-load (system-name)
+(defun quicklisp-load (cl-system-name)
   "Load Quicklisp system."
   (interactive "sQuickload: ")
-  (message "Loading %s..." system-name)
-  (slime-eval `(ql:quickload ,system-name))
-  (message "%s loaded" system-name))
+  (message "Loading %s..." cl-system-name)
+  (slime-eval `(ql:quickload ,cl-system-name))
+  (message "%s loaded" cl-system-name))
 
 (defalias 'quickload 'quicklisp-load)
 
 (defun quicklisp-systems--print-systems-list (systems)
   (dolist (system systems)
     (cl-flet ((show-system (btn)
-                           (quicklisp-systems-show-system (getf system :name))))
-      (insert-text-button (getf system :name)
+		           (ignore btn)
+                           (quicklisp-systems-show-system (cl-getf system :name))))
+      (insert-text-button (cl-getf system :name)
                           'face 'bold
                           'action (function show-system)
                           'follow-link t)
       (newline)
-      (when (and (getf system :description)
-                 (stringp (getf system :description)))
-        (insert (getf system :description))
+      (when (and (cl-getf system :description)
+                 (stringp (cl-getf system :description)))
+        (insert (cl-getf system :description))
         (newline)))))
 
 (defun quicklisp-systems--check-systems-list ()
@@ -175,7 +177,7 @@
     (when (yes-or-no-p "Systems list is empty. Download? ")
       (quicklisp-systems-update))))
 
-(defun* quicklisp-systems-list ()
+(cl-defun quicklisp-systems-list ()
   "Show a buffer with all quicklisp systems"
   (interactive)
   (quicklisp-systems--check-systems-list)
@@ -183,7 +185,7 @@
         (buffer-name "*quicklisp-systems: system list*"))
     (when (get-buffer buffer-name)
       (pop-to-buffer buffer-name)
-      (return-from quicklisp-systems-list))
+      (cl-return-from quicklisp-systems-list))
     (let ((buffer (get-buffer-create buffer-name)))
       (with-current-buffer buffer
         (quicklisp-systems--print-systems-list systems)
@@ -207,47 +209,48 @@
     (quicklisp-systems-mode)
     (pop-to-buffer buffer)))
 
-(defun* quicklisp-systems-show-system (system-name)
-  "Show Quicklisp system SYSTEM-NAME."
+(cl-defun quicklisp-systems-show-system (cl-system-name)
+  "Show Quicklisp system CL-SYSTEM-NAME."
   (interactive "sShow Quicklisp system: ")
-  (let ((system (slime-eval `(quicklisp-systems::find-system-info ,system-name)))
-        (buffer-name (format "*quicklisp-systems: %s*" system-name)))
+  (let ((system (slime-eval `(quicklisp-systems::find-system-info ,cl-system-name)))
+        (buffer-name (format "*quicklisp-systems: %s*" cl-system-name)))
     (when (get-buffer buffer-name)
       (pop-to-buffer buffer-name)
-      (return-from quicklisp-systems-show-system))
+      (cl-return-from quicklisp-systems-show-system))
     (if (null system)
-        (error "Quicklisp system not found: %s" system-name)
+        (error "Quicklisp system not found: %s" cl-system-name)
       (let ((buffer (get-buffer-create buffer-name)))
         (with-current-buffer buffer
-          (insert (propertize (getf system :name) 'face 'quicklisp-systems-title))
+          (insert (propertize (cl-getf system :name) 'face 'quicklisp-systems-title))
           (newline 2)
-          (when (getf system :description)
-            (insert (quicklisp-systems--format-text (getf system :description)))
+          (when (cl-getf system :description)
+            (insert (quicklisp-systems--format-text (cl-getf system :description)))
             (newline 2))
-          (when (getf system :author)
+          (when (cl-getf system :author)
             (insert (propertize "Author: " 'face 'quicklisp-systems-attribute))
-	    (if (stringp (getf system :author))
-		(insert (getf system :author))
-	      (dolist (author (getf system :author))
+	    (if (stringp (cl-getf system :author))
+		(insert (cl-getf system :author))
+	      (dolist (author (cl-getf system :author))
 		(insert author " ")))
             (newline))
-          (when (stringp (getf system :homepage))
+          (when (stringp (cl-getf system :homepage))
             (insert (propertize "Homepage: " 'face 'quicklisp-systems-attribute))
-            (insert (quicklisp-systems--format-text (getf system :homepage)))
+            (insert (quicklisp-systems--format-text (cl-getf system :homepage)))
             (newline))
-          (when (stringp (getf system :bug-tracker))
+          (when (stringp (cl-getf system :bug-tracker))
             (insert (propertize "Bug tracker: " 'face 'quicklisp-systems-attribute))
-            (insert (quicklisp-systems--format-text (getf system :bug-tracker)))
+            (insert (quicklisp-systems--format-text (cl-getf system :bug-tracker)))
             (newline))
-          (when (stringp (getf system :version))
+          (when (stringp (cl-getf system :version))
             (insert (propertize "Version: " 'face 'quicklisp-systems-attribute))
-            (insert (quicklisp-systems--format-text (getf system :version)))
+            (insert (quicklisp-systems--format-text (cl-getf system :version)))
             (newline))
-          (when (getf system :depends-on)
+          (when (cl-getf system :depends-on)
             (insert (propertize "Dependencies: " 'face 'quicklisp-systems-attribute))
-            (dolist (dependency (getf system :depends-on))
+            (dolist (dependency (cl-getf system :depends-on))
               (insert-button dependency
                              'action (lambda (btn)
+				       (ignore btn)
                                        (quicklisp-systems-show-system dependency))
                              'follow-link t)
               (insert " "))
@@ -257,24 +260,26 @@
 	  ;; buttons
           (insert-button "Load"
                          'action (lambda (btn)
-                                   (quicklisp-load system-name))
+				   (ignore btn)
+                                   (quicklisp-load cl-system-name))
                          'follow-link t
                          'help-echo "Load Quicklisp system"
 			 'face 'quicklisp-systems-button)
-	  (when (not (stringp (getf system :homepage)))
+	  (when (not (stringp (cl-getf system :homepage)))
 	    (insert " ")
 	    (insert-button "Search on the internet"
 			   'action (lambda (btn)
-				     (quicklisp-systems-search-on-the-internet system-name))
+				     (ignore btn)
+				     (quicklisp-systems-search-on-the-internet cl-system-name))
 			   'follow-link t
 			   'help-echo "Search the library on the internet"
 			   'face 'quicklisp-systems-button))
 	  (newline 2)
 
-          (when (getf system :long-description)
+          (when (cl-getf system :long-description)
 	    (insert (quicklisp-systems--horizontal-line))
             (newline 2)
-            (insert (quicklisp-systems--format-text (getf system :long-description)))
+            (insert (quicklisp-systems--format-text (cl-getf system :long-description)))
             (newline 2))
 
           (quicklisp-systems--open-buffer))))))
